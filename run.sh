9ce9354982e67d24893fe85c42ea5f9e722b144b
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-09-03 10:32:56 +0200

    Update readme Update run.sh Move docker-compose to separate repo Remove .user.ini

--- run.sh
+++ run.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+echo "[1/5] Go to working directory..."
 cd /var/www/html
 
-echo "Sleeping..."
+echo "[2/5] Creating storage folders..."
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/framework/cache
+mkdir -p storage/logs
+mkdir -p storage/app/public
+mkdir -p storage/app/domain
+mkdir -p storage/app/forms
+mkdir -p storage/app/profiles
+mkdir -p storage/app/temp
+
+echo "[3/5] Sleeping..."
 sleep 5
 
-echo "Migrating..."
+echo "[4/5] Migrating..."
 if [ "$APP_ENV" == "local" ]; then
     php artisan migrate:fresh --seed
 else
     php artisan migrate
 fi
 
-echo "Starting Apache..."
+echo "[5/5] Starting Apache..."
 apache2-foreground
