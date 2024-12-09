93155ed6c2a6cbf373316c5603c82d4585927e61
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-12-09 15:30:37 +0100

    Skip 2fa check if auth via api-token

--- app/Http/Middleware/VerifyTwoFactorAuthentication.php
+++ app/Http/Middleware/VerifyTwoFactorAuthentication.php
@@ -10,8 +10,18 @@ class VerifyTwoFactorAuthentication
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->user()->has_tfa_enabled && !session()->has('two_factor_verified'))
-        {
+        // Skip middleware if request doesn't have session
+        if (!$request->hasSession()) {
+            return $next($request);
+        }
+
+        // Skip middleware if user doesn't have TFA enabled
+        if (!$request->user()->has_tfa_enabled) {
+            return $next($request);
+        }
+
+        // Check if user has verified TFA
+        if (!session()->has('two_factor_verified')) {
             return response()->json(['message' => __('Two factor authentication is required.')], 401);
         }
 
