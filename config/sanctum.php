09cc4b91b4903458842edefb538fe2ba09bbc18a
Alyx Freuwört <33777960+freuwoert@users.noreply.github.com>
2025-05-10 01:13:31 +0200

    Change csrf route

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,6 +13,8 @@ return [
     |
     */
 
+    'prefix' => 'api',
+    
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
